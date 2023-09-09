1411efc3d280d1f71e88305f24bdbcf649ab4fec
Omoyajowo Temidayo <temideewan@gmail.com>
2023-09-09 19:56:17 +0100

    feat: add set up for couses page

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -7,6 +7,8 @@ const Header = () => {
     <nav>
       <NavLink to="/" activeStyle={activeStyle} exact>Home</NavLink>
       {" | "}
+      <NavLink to="/courses" activeStyle={activeStyle}>Courses</NavLink>
+      {" | "}
       <NavLink to="/about" activeStyle={activeStyle} >About</NavLink>
     </nav>
   )
