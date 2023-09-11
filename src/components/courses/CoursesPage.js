3fe5a41868b711c1e39064d50fcdce234c9b116b
Omoyajowo Temidayo <temideewan@gmail.com>
2023-09-11 21:42:51 +0100

    feat: complete first steps to async with thunks in redux

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,28 +1,37 @@
 import React from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
+import CourseList from "./CourseList";
 
 class CoursesPage extends React.Component {
   componentDidMount() {
-    this.props.actions.loadCourses().catch((error) => {
-      alert("Loading courses failed" + error);
-    });
+    const { courses, authors, actions} = this.props;
+    if (courses.length === 0) {
+      actions.loadCourses().catch((error) => {
+        alert("Loading courses failed" + error);
+      });
+    }
+    if (authors.length === 0) {
+      actions.loadAuthors().catch((error) => {
+        alert("Loading authors failed" + error);
+      });
+    }
   }
   render() {
     return (
       <>
         <h2>Courses</h2>
-        {this.props.courses.map((course) => (
-          <div key={course.title}>{course.title}</div>
-        ))}
+        <CourseList courses={this.props.courses} />
       </>
     );
   }
 }
 
 CoursesPage.propTypes = {
+  authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired,
 };
@@ -31,14 +40,24 @@ CoursesPage.propTypes = {
 // second argument and any object returned is spread into the component to be accessible on it's props
 function mapStateToProps(state) {
   return {
-    courses: state.courses,
+    courses:
+      state.authors.length == 0
+        ? []
+        : state.courses.map((course) => ({
+            ...course,
+            authorName: state.authors.find((a) => a.id == course.authorId).name,
+          })),
+    authors: state.authors,
   };
 }
 
 // receives the dispatch and then any object returned is available on the props. when this function is passed in, there's no access to dispatch anymore, only the actions specified on the return object.
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(courseActions, dispatch),
+    actions: {
+      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
+    },
   };
 }
 
