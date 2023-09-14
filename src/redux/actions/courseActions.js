9f3de2adf5d09cd08d58cab5767beb330a0ebd18
Omoyajowo Temidayo <temideewan@gmail.com>
2023-09-14 16:59:51 +0100

    feat: optimistic delete feature

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,6 +1,6 @@
 import * as types from "./actionTypes";
 import * as CourseApi from "../../api/courseApi";
-import {beginApiCall, apiCallError} from "./apiStatusActions";
+import { beginApiCall, apiCallError } from "./apiStatusActions";
 export function createCourse(course) {
   return { type: types.CREATE_COURSE, course };
 }
@@ -13,33 +13,44 @@ export function updateCourseSuccess(course) {
 export function createCourseSuccess(course) {
   return { type: types.CREATE_COURSE_SUCCESS, course };
 }
+export function deleteCourseOptimistic(course) {
+  return { type: types.DELETE_COURSE_OPTIMISTIC, course };
+}
 
 // this is a thunk that returns a function with the dispatch bound to it.
 export function loadCourses() {
   return function (dispatch) {
-    dispatch(beginApiCall())
+    dispatch(beginApiCall());
     return CourseApi.getCourses()
-      .then((courses) => { 
+      .then((courses) => {
         dispatch(loadCoursesSuccess(courses));
       })
       .catch((err) => {
-        dispatch(apiCallError(err))
+        dispatch(apiCallError(err));
         throw err;
       });
   };
 }
 export function saveCourse(course) {
   return function (dispatch) {
-    dispatch(beginApiCall())
+    dispatch(beginApiCall());
     return CourseApi.saveCourse(course)
-      .then((savedCourse) => { 
-        course.id?
-        dispatch(updateCourseSuccess(savedCourse)):
-        dispatch(createCourseSuccess(savedCourse));
+      .then((savedCourse) => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
       })
       .catch((err) => {
-        dispatch(apiCallError(err))
+        dispatch(apiCallError(err));
         throw err;
       });
+  };
+}
+
+export function deleteCourse(course) {
+  return function (dispatch) {
+    dispatch(deleteCourseOptimistic(course));
+
+    return CourseApi.deleteCourse(course.id);
   };
 }
