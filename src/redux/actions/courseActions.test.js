81ae8d0ca16c23d4979541bdb215bb324e9506fb
Omoyajowo Temidayo <temideewan@gmail.com>
2023-09-15 14:46:29 +0100

    feat: test async actions

--- src/redux/actions/courseActions.test.js
+++ src/redux/actions/courseActions.test.js
@@ -1,17 +1,45 @@
 import * as courseActions from "./courseActions";
 import * as types from "./actionTypes";
 import { courses } from "../../../tools/mockData";
+import thunk from "redux-thunk";
+import fetchMock from "fetch-mock";
+import configureMockStore from "redux-mock-store";
+
+// test an async function
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
+describe("Async actions", () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
+  it("should create BEGIN_APO_CALL and LOAD_COURSE_SUCCESS when loading courses", () => {
+    fetchMock.mock("*", {
+      body: courses,
+      headers: { "content-type": "application/json" },
+    });
+
+    const expectedActions = [
+      {type: types.BEGIN_API_CALL  },
+      {type: types.LOAD_COURSES_SUCCESS, courses}
+    ]
+    const store = mockStore({courses: []});
+    return store.dispatch(courseActions.loadCourses()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+});
 
 // arrange -> act -> assert
 describe("createCourseSuccess", () => {
-  it('should create a CREATE_COURSE_SUCCESS action', () => { 
-      // arrange
-      const course = courses[0];
-      const expectedAction = {type: types.CREATE_COURSE_SUCCESS, course};
-      
-      //  act
-      const action = courseActions.createCourseSuccess(course);
+  it("should create a CREATE_COURSE_SUCCESS action", () => {
+    // arrange
+    const course = courses[0];
+    const expectedAction = { type: types.CREATE_COURSE_SUCCESS, course };
 
-      expect(action).toEqual(expectedAction);
-   });
+    //  act
+    const action = courseActions.createCourseSuccess(course);
+
+    expect(action).toEqual(expectedAction);
+  });
 });
