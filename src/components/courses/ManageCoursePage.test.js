30552a48072215ad3fae7d219603f5e937791297
Omoyajowo Temidayo <temideewan@gmail.com>
2023-09-15 13:43:10 +0100

    feat: create tests for actions

---
+++ src/components/courses/ManageCoursePage.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {ManageCoursePage} from "./ManageCoursePage";
+import { mount } from "enzyme";
+import { authors, newCourse, courses } from "../../../tools/mockData";
+
+function render(args) {
+  const defaultProps = {
+    authors,
+    courses,
+    course: newCourse,
+    history: {},
+    saveCourse: jest.fn(),
+    loadAuthors: jest.fn(),
+    loadCourses: jest.fn(),
+    match: {},
+  };
+
+  const props = { ...defaultProps, ...args };
+
+  return mount(<ManageCoursePage {...props} />);
+}
+
+it("sets error when attempting to save an empty title field", () => {
+  const wrapper = render();
+  wrapper.find("form").simulate("submit");
+  const error = wrapper.find(".alert").first();
+  expect(error.text()).toBe("Title is required.");
+});
